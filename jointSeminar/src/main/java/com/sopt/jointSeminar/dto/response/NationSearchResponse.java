ea388c281b6459cc993d1eaf08ea42521a56f10f
gardening-y <60008786@naver.com>
2023-11-21 21:43:30 +0900

    Add response dto file that receive nation

---
+++ jointSeminar/src/main/java/com/sopt/jointSeminar/dto/response/NationSearchResponse.java
@@ -0,0 +1,21 @@
+package com.sopt.jointSeminar.dto.response;
+
+import com.sopt.jointSeminar.domain.Nation;
+
+public record NationSearchResponse(
+        Long nationId,
+        String nationName,
+        String itatCode,
+        String city) {
+
+    public static NationSearchResponse of(Nation nation) {
+        return new NationSearchResponse(
+                nation.getNationId(),
+                nation.getNationName(),
+                nation.getItatCode(),
+                nation.getCity()
+        );
+    }
+
+}
+
