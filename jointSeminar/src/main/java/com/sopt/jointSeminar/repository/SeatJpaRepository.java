84ba23ee9d8cebae460cfb7e5c0ec4667da4d837
임주민 <limj1213@gmail.com>
2023-11-26 18:50:30 +0900

    Add jpa repository files

---
+++ jointSeminar/src/main/java/com/sopt/jointSeminar/repository/SeatJpaRepository.java
@@ -0,0 +1,11 @@
+package com.sopt.jointSeminar.repository;
+
+import com.sopt.jointSeminar.domain.Seat;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SeatJpaRepository extends JpaRepository<Seat, Long> {
+
+    List<Seat> findByFlightFlightId(Long flightId);
+}
