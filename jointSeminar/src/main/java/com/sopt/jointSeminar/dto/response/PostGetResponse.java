33dbdfa9e5b450e36399724224ae0ad00a343070
임주민 <limj1213@gmail.com>
2023-11-26 18:52:17 +0900

    Delete not necessary codes

--- jointSeminar/src/main/java/com/sopt/jointSeminar/dto/response/PostGetResponse.java
+++ jointSeminar/src/main/java/com/sopt/jointSeminar/dto/response/PostGetResponse.java
@@ -2,8 +2,6 @@ package com.sopt.jointSeminar.dto.response;
 
 import com.sopt.jointSeminar.domain.Post;
 
-import java.time.LocalDateTime;
-
 public record PostGetResponse(
     Long id,
     String title,
